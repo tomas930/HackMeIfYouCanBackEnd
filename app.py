602eaeec9f85daf721d690ee9d361b4868657ac1
Tomasz Okraska <okraskat@volt.iem.pw.edu.pl>
2014-06-03 23:35:32 +0200

    Add methods to cooperating with db

--- app.py
+++ app.py
@@ -4,45 +4,109 @@
 from gevent import monkey; monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 import time
-import web
 import os
+import sqlite3
 
-urls = ('/', "index",
-        '/long', 'long_polling',
-        '/check', 'check')
+database = "database.db"
 
+def connect():
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    return conn, c
+	
+def getLastID():
+    conn, cursor = connect()
+    cursor.execute("select id from notes;")
+    ids = cursor.fetchall()
+    lastID = 0
+    conn.close()
+    for id in ids:
+        tmp = id[0]
+        if tmp > lastID:
+            lastID = tmp
+    return lastID
 
-class index:
-    def GET(self):
-        return '<html>Hello, world!<br><a href="/long">/long</a></html>'
-
-
-class long_polling:
-    # Since gevent's WSGIServer executes each incoming connection in a separate greenlet
-    # long running requests such as this one don't block one another;
-    # and thanks to "monkey.patch_all()" statement at the top, thread-local storage used by web.ctx
-    # becomes greenlet-local storage thus making requests isolated as they should be.
-    def GET(self):
-        print 'GET /long'
-        time.sleep(10)  # possible to block the request indefinitely, without harming others
-        return 'Hello, 10 seconds later'
-
-class check:
-    # Since gevent's WSGIServer executes each incoming connection in a separate greenlet
-    # long running requests such as this one don't block one another;
-    # and thanks to "monkey.patch_all()" statement at the top, thread-local storage used by web.ctx
-    # becomes greenlet-local storage thus making requests isolated as they should be.
-    def GET(self):
-        print 'GET /check'
-        while not os.path.exists('/home/ee/stud/ficm/www/plik'):
-             time.sleep(0.5)
-        return 'Plik sie pojawil\n'
-
-        
-        
-
+noteIDcounter = getLastID()
+	
+def addFile(login, userFile):
+    conn, cursor = connect()
+    conn.close()
+    return 0
+def addNote(noteID, login, note):
+    conn, cursor = connect()
+    try:
+        cursor.execute("insert into notes values(\'"+noteID+"\',\'"+login+"\',\'"+note+"\');")
+    except sqlite3.IntegrityError:
+        return False
+    conn.commit()
+    conn.close()
+    noteIDcounter += 1
+    return True
+	
+def getUserNotes(login):
+    conn, cursor = connect() 
+    try:
+        cursor.execute("select note from notes where login=\'"+login+"\';")
+        rows = cursor.fetchall()
+	if len(rows) == 0:
+            return []
+        else:
+            response = []
+            for row in rows:
+                response.append(row[0])
+            return response
+    except sqlite3.IntegrityError:
+        return []
+	
+def getUserFile( login ):
+    conn, cursor = connect()
+    conn.close()
+    return 0
+	
+def getUserInformation(login):
+    conn, cursor = connect()
+    cursor.execute("select * from users where login=\'"+login+"\';")
+    info = cursor.fetchone()
+    conn.close()
+    return info
+	
+def checkPassword( login, password):
+    conn, cursor = connect()
+    cursor.execute('select password from users where login=\'' + login + "\';")
+    result = cursor.fetchone()
+    conn.close()
+    result = result[0]
+    if result == password:
+        return True
+    return False
+	
+def addErrorLog( date, message):
+    conn, cursor = connect()
+    conn.close()
+    return 0
+	
+def addLog( date, message):
+    conn, cursor = connect()
+    conn.close()
+    return 0
+	
+def checkIfLoginIsFree(login):
+    conn, cursor = connect()
+    cursor.execute("select login from users where login=\'"+login+"\';")
+    rows = cursor.fetchone()
+    conn.close()
+    result = rows[0]
+    if result == login:
+        return False
+    return True
+def addUserToDB( login, password, email, name, surname):
+    conn, cursor = connect()
+    try:
+        cursor.execute("insert into users values(\'"+login+"\',\'"+password+"\',\'"+email+"\',\'"+name+"\',\'"+surname+"\');")
+    except sqlite3.IntegrityError:
+        return False
+    conn.commit()
+    conn.close()
+    return True
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    wsgi = app.wsgifunc()
-    print "Działam na porcie 8081"
-    WSGIServer(('', 8081), wsgi).serve_forever()
+    print getUserInformation('python')
