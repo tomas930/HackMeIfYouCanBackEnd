5857d73bf0bdbbeebd0d220f69fac49b65002114
Tomasz Okraska <okraskat@volt.iem.pw.edu.pl>
2014-06-08 18:42:41 +0200

    Add uploading file to server and delay after 5 login retries

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from sessionControler import *
 
 # allowed methods in dbConnector 
 # getLastID()	return lastID
-# addFile(login, userFile)	return True
+# addFile(self,login, userFile, extension):
 # getUserFile(self,login):	return file
 # addNote(self,noteID, login, note):	return True
 # getUserNotes(self,login):	return []
@@ -35,6 +35,8 @@ prefix = '/~okraskat/apps/safetyNote'
 urls = (prefix + '/notes/(.*)', 'Notes',
         prefix + '/register/(.*)', 'Register',
         prefix + '/login/(.*)', 'Login',
+        prefix + '/reset', 'ResetPassword',
+        prefix + "/upload", 'Upload',
         prefix + '/(.*)', 'Index',		
         )
 		
@@ -45,7 +47,50 @@ session = SessionController(app)
 connector = dbConnector()
 
 noteIDcounter = connector.getLastID()
+passwordReset = ''
+badLoginCounter = 0
 
+class Upload:
+    def GET(self):
+        return """<html><head></head><body>
+<form method="POST" enctype="multipart/form-data" action="">
+<input type="file" name="myfile" />
+<br/>
+<input type="submit" />
+</form>
+</body></html>"""
+
+    def POST(self):
+        x = web.input(myfile={})
+        file = open('/home/stud/~okraskat/database/tmp', 'w')
+        file.write(x['myfile'].file.read())
+        file.close()
+        # TODO insert page address to redirect
+        raise web.seeother('')
+		
+		
+class ResetPassword:
+    def GET(self):
+        data = web.data()
+        data = json.loads(data)
+        info = connector.getUserInformation(data['login'])
+        passwordReset = ''.join(random.sample(string.ascii_letters, 12))
+        sendMail(info[2], 'Password reset', 'Yor password reset key is:' + passwordReset)
+        return 0
+    def POST(self):
+        data = web.data()
+        data = json.loads(data)
+        
+        if data['resetKey'] == passwordReset:
+            salt = connector.getSalt(login)
+            password = crypt.crypt(data['password'], salt)
+            for i in range(1, 10):
+                password = crypt.crypt(password, salt)		
+            connector.updateUserPassword(data['login'], password)
+            return json.dumps({'result' : True})
+        else:
+            return json.dumps({'result' : 'Invalid reset key.'})
+        
 class Login:
     def POST(self):
         data = web.data()
@@ -61,6 +106,11 @@ class Login:
         if logged == True:
             session.setSession(login)
             web.setcookie('login', login, 3600, secure=True )
+        else:
+            if badLoginCounter == 5:
+                badLoginCounter = 0
+                time.sleep(60)
+            badLoginCounter = badLoginCounter + 1
         response = json.dumps(response)
         return response
 
