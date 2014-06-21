54843b0d54d506ed3b3cbd182a24b5e762d95e2a
Maciej Fic <ficm@volt.iem.pw.edu.pl>
2014-06-21 21:23:45 +0200

    Added new email as bcc in mail sender

--- mail.py
+++ mail.py
@@ -5,13 +5,13 @@ from email.mime.text import MIMEText
 class mail:
     def __init__(self):
         return
-    def send(self, sendTo, message, topic):
+    def send(self, sendTo, topic, message):
         msg = MIMEText(message)
 
         msg['Subject'] = topic
         msg['From'] = 'HackMeIfYouCanTeam@fake.com'
         msg['To'] = 'tomas930@vp.pl'
-
+        msg['BCC'] = 'mcprogamer@gmail.com'
         s = smtplib.SMTP('localhost')
         s.sendmail('HackMeIfYouCanTeam@fake.com', sendTo, msg.as_string())
         s.quit()
